65cf86d5e1e52660e0c374f3c86fcbeae462629c
Antoine <me@antoinebellanger.fr>
2025-01-31 11:07:14 +0100

    feat: ex01

---
+++ Array.hpp
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2025
+** paradigm-day13
+** File description:
+** Array.hpp
+*/
+
+#ifndef ARRAY_HPP
+    #define ARRAY_HPP
+
+    #include <functional>
+
+template <typename T, std::size_t Size>
+class Array
+{
+    public:
+        T& operator[](std::size_t idx)
+        {
+            if (idx >= Size)
+                throw std::out_of_range("Out of range");
+            return this->_array[idx];
+        }
+
+        std::size_t size() const { return Size; }
+
+        void forEach(const std::function<void(const T&)>& task) const
+        {
+            for (std::size_t i = 0; i < Size; i++) { task(this->_array[i]); }
+        }
+
+        template<typename U>
+        Array<U, Size> convert(const std::function<U(const T&)>& converter) const
+        {
+            Array<U, Size> array;
+            for (std::size_t i = 0; i < Size; i++) { array[i] = converter(this->_array[i]); }
+            return array;
+        }
+
+    private:
+        std::array<T, Size> _array;
+};
+
+template <typename T, std::size_t Size>
+std::ostream& operator<<(std::ostream& os, Array<T, Size>& array)
+{
+    os << '[';
+    for (std::size_t i = 0; i < Size; i++) {
+        if (i != 0)
+            os << ", ";
+        os << array[i];
+    }
+    os << ']';
+    return os;
+}
+#endif //ARRAY_HPP
--- bonus/main.cpp
+++ bonus/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 
 #include "Algorithm.hpp"
+#include "Array.hpp"
 
 int main_ex00()
 {
@@ -29,7 +30,27 @@ int main_ex00()
     return 0;
 }
 
+int main_ex01()
+{
+    Array<int, 3> array;
+    try
+    {
+        array[0] = 21;
+        array[1] = 42;
+        array[2] = 84;
+        array[3] = 84;
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << array << std::endl;
+    array
+        .convert<float>([](const int& v) { return static_cast<float>(v) / 10.f; })
+        .forEach([](const float& v) { std::cout << v << std::endl; });
+    return 0;
+}
+
 int main()
 {
-    return main_ex00();
+    //return main_ex00();
+    return main_ex01();
 }
